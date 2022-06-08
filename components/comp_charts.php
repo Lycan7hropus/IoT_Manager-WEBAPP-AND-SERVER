b72e12645d69a95d3e699b61e8c5d57340085963
Antek Zwolski <zwo7ak@gmail.com>
2022-06-08 20:32:40 +0200

    created folders commit

---
+++ components/comp_charts.php
@@ -0,0 +1,19 @@
+<div style="width:96%; margin-left:auto; margin-right:auto;">
+        <div class="row justify-content-center ">
+            <div class="m-2 col-md-8 col-lg-6 col-xl-4 card p-2 shadow-sm">
+                <div class="myChart" id="TempChart">
+                    <div id="chartContainerTemp" style="height: 333px; width: 100%;"></div>
+                </div>
+            </div>
+            <div class="m-2 col-md-8 col-lg-6 col-xl-4 card p-2 shadow-sm">
+                <div class="myChart" id="TempChart">
+                    <div id="chartContainerHum" style="height: 333px; width: 100%;"></div>
+                </div>
+            </div>
+            <div class="m-2 col-md-8 col-lg-6 col-xl-4 card p-2 shadow-sm">
+                <div class="myChart" id="TempChart">
+                    <div id="chartContainerPres" style="height: 333px; width: 100%; "></div>
+                </div>
+            </div>
+        </div>
+</div>
